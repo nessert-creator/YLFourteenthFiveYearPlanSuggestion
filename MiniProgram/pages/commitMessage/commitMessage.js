79fcc261f027dd4c137ff66255e96167f6087720
nessert <nessert@163.com>
2020-09-22 11:18:26 +0800

    提交小程序数据改动

---
+++ MiniProgram/pages/commitMessage/commitMessage.js
@@ -0,0 +1,52 @@
+//logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+  },
+  onLoad: function () {
+    
+  },
+  commitMessage:function(){
+    const page = this;
+    wx.request({
+      url: consts.remoteUrl + '/api/services/app/message/CommitMessage',
+      data:{
+        "name": "string",
+        "email": "string",
+        "phoneNumber": "string",
+        "identityType": 1,
+        "organization": "string",
+        "messages": [
+          {
+            "topicId": 0,
+            "message": "string"
+          }
+        ]
+      },
+      method: "POST",
+      success: function (res) {
+        if (res.statusCode == 200) {
+          page.setData({
+            showLoading:false
+          })
+          
+          wx.showToast({
+            title:  "您的建议我们已收到，感谢您对本活动的支持！",
+            duration: 2000
+          });
+
+        }else{
+          wx.showToast({
+            title:  res.data.error.message,
+            icon: 'none',
+            duration: 2000
+          });
+          page.setData({
+            showLoading:false
+          })
+        }
+      }
+    })
+  }
+})
---
+++ MiniProgram/utils/consts.js
@@ -0,0 +1,2 @@
+// exports.remoteUrl = "http://localhost:6234"
+exports.remoteUrl = "http://220.165.143.99:6998/"
